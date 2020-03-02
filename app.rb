4b0989b9f9a8d772f7b569873a5259ed2d4bdc95
Olivia Thetgyi <othetgyi@gmail.com>
2020-03-02 16:48:31 +0000

    I made an HTML file

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-# require 'shotgun'
+
 
 set :session_secret, 'super secret'
 
@@ -12,16 +12,5 @@ get '/secret' do
 end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-  <img src='http://bit.ly/1eze8aE'>
-  </div>"
+  erb(:index)
 end
-
-
-or create a new repository on the command line
-echo "# web" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/othetgyi/web.git
-git push -u origin master
