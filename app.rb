e53a79e4dab9def917a078a60d529a20a3936d68
Olivia Thetgyi <othetgyi@gmail.com>
2020-03-03 14:15:34 +0000

    Params

--- app.rb
+++ app.rb
@@ -11,7 +11,14 @@ get '/secret' do
   "Happy"
 end
 
-get '/cat' do
+get '/random-cat' do
+
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb :index
 end
